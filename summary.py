1d01cc385c669209057bbcb34fe38f7c2b7a54b4
Avijit Das <140038956+avijitdas126@users.noreply.github.com>
2025-02-03 11:02:28 +0530

    Update summary.py

--- summary.py
+++ summary.py
@@ -12,7 +12,7 @@ os.makedirs(nltk_data_path, exist_ok=True)
 nltk.data.path.append(nltk_data_path)
 
 # Download the required tokenizer
-nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('punkt_tab', download_dir=nltk_data_path)
 
 print("NLTK data is ready!")
 
